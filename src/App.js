ec5aec1075250c8d732ba846bdcdc9b67414ca29
jade <jade@jaderose.red>
2020-12-26 13:06:37 -0500

    toCss working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createRef } from "react";
 
+const testStyle = {
+  margin: 0,
+  padding: 0,
+  background: "none",
+  fontSize: "1vw",
+  top: ".2vw",
+  fontWeight: "bold",
+  left: "2%",
+  color: "blue",
+  position: "absolute",
+  width: "92%",
+};
+
+const text = {
+  value: "",
+  toCss() {
+    const findIllegal = /[="]/gi;
+    const findComma = /[,]/gi;
+    const findUppercase = /[A-Z]/g;
+  
+    let filteredString = this.value
+      .replace(findIllegal, "")
+      .replace(findComma, "; ")
+      .replace(findUppercase, (char) => `-${char.toLowerCase()}`);
+
+    const openBracketIndex = filteredString.indexOf("{") || false;
+    const className =
+      openBracketIndex && openBracketIndex != 0
+        ? `.${filteredString.split(" ")[1]}`
+        : false;
+    filteredString = `${className ? className : ""} ${filteredString.slice(openBracketIndex)}`;
+    return filteredString;
+    console.log("filteredString " + filteredString);
+    console.log("classname " + className);
+    // return textArray.map((text) => {});
+  },
+  updateValue(input) {
+    this.value = JSON.stringify(input);
+  },
+};
 function App() {
+  const leftInput = createRef();
+  const rightInput = createRef();
+
+  text.updateValue(testStyle);
+  text.toCss();
+
+  const handleLeftInput = ({ target }) => {
+    const { value } = target;
+    console.log(value);
+  };
+
+  const handleRightInput = ({ target }) => {
+    const { value } = target;
+    console.log(value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" ref={leftInput} onChange={handleLeftInput} />
+      <input type="text" ref={rightInput} onChange={handleRightInput} />
+      <code>{text.toCss()}</code>
     </div>
   );
 }
