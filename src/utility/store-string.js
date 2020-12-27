e785470c46f4fb08a088f999fd7f6d34219922fc
jade <jade@jaderose.red>
2020-12-27 11:06:08 -0500

    importing an existing code editor instead of making one from scratch

--- src/utility/store-string.js
+++ src/utility/store-string.js
@@ -1,13 +1,19 @@
 const findIllegal = /[="\\']/gi;
 const findComma = /[,]/gi;
 const findUppercase = /[A-Z]/g;
-const findLineBreak = /(\r\n|\n|\r)/gm;
+// const findLineBreak = /(\r\n|\n|\r)/gm;
 const findDoubleSpaces = / +(?= )/g;
 const findEndBracket = /[}]/g;
 const newLineTrigger = /[{};]/g;
 const tempChar = "?";
+const findComments = /\/\*[\s\S]*?\*\/|\/\/.*/g;
 
 function ManipulateString({ mode, value }) {
+  const comments = [];
+  const convertComment = (comment, index) => {
+    comments.push({ comment: comment, index: index });
+    return `/*${comment.replace("//", "")}*/`;
+  };
   const toCss = () => {
     //allows split by end bracket without cutting it away from the string
     const parts = value
@@ -15,9 +21,9 @@ function ManipulateString({ mode, value }) {
       .split(tempChar);
     const filteredArray = parts.map((part) => {
       part = part
-        .replace(findLineBreak, "")
-        .replace(findDoubleSpaces, "")
         .trim()
+        .replace(findComments, convertComment)
+        .replace(findDoubleSpaces, "")
         .replace(findIllegal, "")
         .replace(findComma, ";")
         .replace(findUppercase, (char) => `-${char.toLowerCase()}`);
@@ -25,9 +31,23 @@ function ManipulateString({ mode, value }) {
       const openBracketIndex =
         part.indexOf("{") > 0 ? part.indexOf("{") : false;
 
-      const className = openBracketIndex ? `.${part.split(" ")[1]}` : false;
-      part = `${className ? className : ""} ${part.slice(openBracketIndex)}`;
+      
+      const className =
+        part
+          .slice(0, openBracketIndex)
+          .split(" ")
+          .filter((string) => string.trim())
+          .reverse()[0] || false;
+        let preceedingComments = part.slice(0, openBracketIndex);
+        const endIndex = preceedingComments.lastIndexOf("*/")
+        console.log("end index " + endIndex)
+        preceedingComments = preceedingComments.slice(0, endIndex + 2) 
 
+
+      part = `${preceedingComments} ${className ? "." + className : ""} ${part.slice(
+        openBracketIndex
+      )}`;
+      console.log(part)
       return part;
     });
     return filteredArray.join("");
@@ -46,21 +66,31 @@ function ManipulateString({ mode, value }) {
         .filter((string) => string.length > 0 && string !== ";");
 
       jsx = stringArray.map((string, key) => {
-          //detects whether the string is the opening or closing bracket lines
-          const isFirstOrLast = string.search(/[{}]/) > -1 ? true : false;
-          const cssRuleIndex = string.search()
-          console.log(isFirstOrLast);
-        
+        //detects whether the string is the opening or closing bracket lines
+        let comment, cssRule, ruleValue;
+        const isFirstOrLast = string.search(/[{}]/) > -1 ? true : false;
+        const commentIndex = string.search("//");
+        const hasComment = commentIndex > -1 ? true : false;
+        const colonIndex = string.search(":");
+        const hasColon = colonIndex > -1 ? true : false;
+
+        console.log(isFirstOrLast);
+
         return (
           <>
-            {!isFirstOrLast ? <>&nbsp;&nbsp;&nbsp;&nbsp;</> : ""}{string} <br />
+            {!isFirstOrLast ? <>&nbsp;&nbsp;&nbsp;&nbsp;</> : ""}
+            {string} <br />
           </>
         );
       });
       console.log(jsx);
     }
 
-    return <div className="box">{jsx}</div>;
+    return (
+      <div contenteditable="true" className="box">
+        {jsx}
+      </div>
+    );
   };
 
   return formatAsJsx();
